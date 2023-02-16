27bf5ae2e4dab9b9771f13b1bd86f86326f6c100
jieun <wldmsdlsns1@gmail.com>
2023-02-16 10:12:35 +0900

    카카오 로그인

--- src/App.js
+++ src/App.js
@@ -14,36 +14,52 @@ import Gallery from "./components/sub/Gallery";
 import Youtube from "./components/sub/Youtube";
 import Location from "./components/sub/Location";
 import Join from "./components/sub/Join";
+import Login from "./components/sub/Login";
+import Logout from "./components/sub/Logout";
 
 function App() {
-  return (
-    <>
-      {/* 화면에 중첩되는 컴포넌트가 있는 경우 Switch를 활용한다. */}
-      <Switch>
-        <Route exact path="/">
-          {/* 라우터 값에 따라서 Header props로 type="main" */}
-          <Header type={"main"} />
-          <Visual />
-          <Content />
-        </Route>
+    // 카카오 로그인 기능
+    // 등록된 앱의 JavaScript key
+    const jsKey = process.env.REACT_APP_KAKAO;
 
-        {/* 라우터에 따라서 header의 css를  달리하겠다. */}
-        {/* 중첩되는 Header 에 대한 처리가 필요하다. */}
-        {/* 컴포넌트를 출력하는 3번째 방법 */}
-        {/* <Header type={"sub"}/> */}
-        <Route path="/" render={() => <Header type={"sub"} />} />
-      </Switch>
-      <Route path="/department" component={Department} />
-      <Route path="/community" component={Community} />
-      <Route path="/schedule" component={Schedule} />
-      <Route path="/gallery" component={Gallery} />
-      <Route path="/youtube" component={Youtube} />
-      <Route path="/location" component={Location} />
-      <Route path="/join" component={Join} />
+    // SDK는 한 번만 초기화해야 한다.
+    // 중복되는 초기화를 막기 위해 isInitialized()로 SDK 초기화 여부를 판단한다.
+    if (!window.Kakao.isInitialized()) {
+        // JavaScript key를 인자로 주고 SDK 초기화
+        window.Kakao.init(jsKey);
+        // SDK 초기화 여부를 확인하자.
+        console.log(window.Kakao.isInitialized());
+    }
+    return (
+        <>
+            {/* 화면에 중첩되는 컴포넌트가 있는 경우 Switch를 활용한다. */}
+            <Switch>
+                <Route exact path="/">
+                    {/* 라우터 값에 따라서 Header props로 type="main" */}
+                    <Header type={"main"} />
+                    <Visual />
+                    <Content />
+                </Route>
 
-      <Footer />
-    </>
-  );
+                {/* 라우터에 따라서 header의 css를  달리하겠다. */}
+                {/* 중첩되는 Header 에 대한 처리가 필요하다. */}
+                {/* 컴포넌트를 출력하는 3번째 방법 */}
+                {/* <Header type={"sub"}/> */}
+                <Route path="/" render={() => <Header type={"sub"} />} />
+            </Switch>
+            <Route path="/department" component={Department} />
+            <Route path="/community" component={Community} />
+            <Route path="/schedule" component={Schedule} />
+            <Route path="/gallery" component={Gallery} />
+            <Route path="/youtube" component={Youtube} />
+            <Route path="/location" component={Location} />
+            <Route path="/join" component={Join} />
+            <Route path="/login" component={Login} />
+            <Route path="/logout" component={Logout} />
+
+            <Footer />
+        </>
+    );
 }
 
 export default App;
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -5,55 +5,60 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Header = (props) => {
-  const active = { color: "hotpink" };
-  return (
-    <header className={props.type}>
-      <div className="inner">
-        <h1>
-          <Link to="/">Logo</Link>
-        </h1>
-        <ul id="gnb">
-          <li>
-            <NavLink activeStyle={active} to="/department">
-              {" "}
-              Department
-            </NavLink>
-          </li>
-          <li>
-            <NavLink activeStyle={active} to="/community">
-              Community
-            </NavLink>
-          </li>
-          <li>
-            <NavLink activeStyle={active} to="/schedule">
-              Schedule
-            </NavLink>
-          </li>
-          <li>
-            <NavLink activeStyle={active} to="/gallery">
-              Gallery
-            </NavLink>
-          </li>
-          <li>
-            <NavLink activeStyle={active} to="/youtube">
-              Youtube
-            </NavLink>
-          </li>
-          <li>
-            <NavLink activeStyle={active} to="/location">
-              Location
-            </NavLink>
-          </li>
-          <li>
-            <NavLink activeStyle={active} to="/join">
-              Join
-            </NavLink>
-          </li>
-        </ul>
-        <FontAwesomeIcon icon={faBars} className="fa-bars" />
-      </div>
-    </header>
-  );
+    const active = { color: "hotpink" };
+    return (
+        <header className={props.type}>
+            <div className="inner">
+                <h1>
+                    <Link to="/">Logo</Link>
+                </h1>
+                <ul id="gnb">
+                    <li>
+                        <NavLink activeStyle={active} to="/department">
+                            {" "}
+                            Department
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeStyle={active} to="/community">
+                            Community
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeStyle={active} to="/schedule">
+                            Schedule
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeStyle={active} to="/gallery">
+                            Gallery
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeStyle={active} to="/youtube">
+                            Youtube
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeStyle={active} to="/location">
+                            Location
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeStyle={active} to="/join">
+                            Join
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeStyle={active} to="/login">
+                            Login
+                        </NavLink>
+                    </li>
+                </ul>
+                <FontAwesomeIcon icon={faBars} className="fa-bars" />
+            </div>
+        </header>
+    );
 };
 
 export default Header;
